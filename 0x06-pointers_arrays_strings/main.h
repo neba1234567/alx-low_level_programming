47e5087b2b31591eb1474bb4884f057c8d25b47b
Nebil Abdulfetah <nebilabdu300@gmail.com>
2022-03-24 22:40:38 +0300

    fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+char *_strcat(char *dest, char *src);
 #include <stdio.h>
-
-void positive_or_negative(int n);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
-#endif /* MAIN_H */
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
